8826aa2e9151c13b3b6505bb479c5d970da7a3f2
ice <iceliang@easyops.cn>
2020-04-16 20:06:52 +0800

    chore(): test and update deps

--- bricks/user-admin/src/data-providers/FakerList.spec.ts
+++ bricks/user-admin/src/data-providers/FakerList.spec.ts
@@ -1,9 +1,13 @@
 // Ref https://github.com/jsdom/jsdom/issues/1030
 import "document-register-element";
 import { FakerList, FakerListParams } from "./FakerList";
+import { http } from "@easyops/brick-http";
+
+jest.mock("@easyops/brick-http");
+jest.spyOn(http, "get").mockResolvedValue({ data: [] });
 
 describe("FakerList", () => {
-  it.each<[FakerListParams, any]>([[{}, null]])(
+  it.each<[FakerListParams, any]>([[{}, { list: [] }]])(
     "FakerList(%j) should work",
     async (params, result) => {
       expect(await FakerList(params)).toEqual(result);
